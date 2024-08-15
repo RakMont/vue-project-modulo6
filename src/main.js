9321bfaf74835349a8cf0504e23bafb18da69a3c
RakMont <nabogaria@gmail.com>
2024-08-14 20:15:30 -0400

    Dependencias iniciales

--- src/main.js
+++ src/main.js
@@ -2,5 +2,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import toasts from './toasts';
+import './assets/styles/styles.scss'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App);
+app.use(store);
+app.use(router);
+app.use(VueAxios, axios);
+app.mount('#app')
+app.use(toasts);
+
+//createApp(App).use(store).use(router).mount('#app')
--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,25 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    baseUrl: process.env.VUE_APP_API_URL
   },
   getters: {
+    doubleCount(state) {
+      return state.count * 2
+    },
+    getBaseUrl(state) {
+      return state.baseUrl
+    },
   },
   mutations: {
+    increment(state) {
+      state.count++
+    }
   },
   actions: {
+     increment({ commit }) {
+      commit('increment')
+    }
   },
   modules: {
   }
